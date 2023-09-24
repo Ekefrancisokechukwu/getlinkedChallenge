114ae7648ebb8b264bb94e1c4efad533745d8876
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2023-09-24 11:43:08 +0100

    Update SharedLayouts.jsx

--- src/pages/SharedLayouts.jsx
+++ src/pages/SharedLayouts.jsx
@@ -1,10 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
 import { Header } from "../components";
 import btn from "../assets/images/btn.png";
+import { useNavigation } from "react-router-dom";
 
 const SharedLayouts = () => {
+   const navigation = useNavigation();
+  const isNavigating = navigation.state === "loading";
   return (
     <>
+       {isNavigating && (
+        <div className="fixed z-[40] top-0 left-0 w-screen h-screen bg-main/70">
+          <div className="h-5 w-5  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin rounded-full border-b-2 border-gray-100"></div>
+        </div>
+      )}
       <div className="tablet:block hidden">
         <Header />
       </div>
