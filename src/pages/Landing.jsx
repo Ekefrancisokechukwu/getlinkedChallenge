793c48153e3d8ca29b61e9e8d90a6d6a90636165
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2023-09-24 09:26:30 +0100

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -12,10 +12,19 @@ import {
   PrivacySection,
   TimeLineSection,
 } from "../components";
+import { useNavigation } from "react-router-dom";
+
 
 const Landing = () => {
+   const navigation = useNavigation();
+  const isNavigating = navigation.state === "loading";
   return (
     <>
+       {isNavigating && (
+        <div className="fixed z-[40] top-0 left-0 w-screen h-screen bg-main/70">
+          <div className="h-5 w-5  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-spin rounded-full border-b-2 border-gray-100"></div>
+        </div>
+      )}
       <Menu />
       <Header />
       <main>
